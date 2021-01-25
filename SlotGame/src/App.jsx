ccc7d73867b868b0a0020c6024333c92c661ebf5
SARVESH <sarvesh.@SARVESHs-MacBook-Pro.local>
2021-01-25 23:47:45 +0530

    Slot machine

--- SlotGame/src/App.jsx
+++ SlotGame/src/App.jsx
@@ -1,1 +1,18 @@
 import React from "react";
+import SlotM from "./Slotmach";
+
+const App=() =>{
+return(
+<>
+    <h1 className="heading_style"> 
+    🎰 Welcome to {" "}
+    <span style={{ fontWeight: "bold" }}> Slot machine game </span> 🎰 
+    </h1>
+    <SlotM x= '😄' y= '😄' z= '😄'/>
+    <SlotM x= '😄' y= '😄' z='🎅'/>
+    <SlotM x='🍌' y='🍌' z='🍌'/>
+</>
+)
+}
+
+export default App;
